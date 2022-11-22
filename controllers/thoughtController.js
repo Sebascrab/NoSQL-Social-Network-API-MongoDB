f11f909df9abfd361525ae2edf6bdd2e56ec0d5e
Alexander <alexanders.tischner@outlook.com>
2022-11-22 12:14:00 -0700

    fixed bugs

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,7 +1,7 @@
 
 
 
-const { Thought } = require('../models');
+const { Thought, User } = require('../models');
 
 module.exports = {
 
@@ -16,15 +16,18 @@ module.exports = {
         Thought.findOne({ _id: req.params.Id })
             .select('-__v')
             .then((thought) => !thought ? res.status(404).json({ message: 'No thought with that ID ' })
-                : res.json(user)
+                : res.json(thought)
             )
             .catch((err) => res.status(500).json(err.message));
     },
 
     createThought(req, res) {
         Thought.create(req.body)
-            .then((thought) => res.json(thought))
-            .catch((err) => res.status(500).json(err));
+            .then((thought) => {
+                User.findOneAndUpdate({ username: req.body.username}, { $addToSet: { thoughts: thought._id }})
+                .then(() => res.json(thought))
+            })
+            .catch((err) => res.status(500).json(err.message));
     },
 
     updateThought(req, res) {
@@ -45,13 +48,13 @@ module.exports = {
     },
 
     createReaction(req, res) {
-        Thought.findByIdAndUpdate(req.params.thoughtId, { $addToSet: { reaction: req.body } }, { runValidators: true, new: true })
+        Thought.findByIdAndUpdate(req.params.thoughtId, { $addToSet: { reactions: req.body } }, { runValidators: true, new: true })
             .then(() => res.json({ message: 'Reaction created' }))
             .catch((err) => res.status(500).json(err));
     },
 
     deleteReaction(req, res) {
-        Thought.findByIdAndUpdate(req.params.thoughtId, { $pull: { reaction: { reactionId: req.params.reactionId } }}, { runValidators: true, new: true })
+        Thought.findByIdAndUpdate(req.params.thoughtId, { $pull: { reactions: { reactionId: req.params.reactionId } }}, { runValidators: true, new: true })
         .then(() => res.json({ message: 'Reaction deleted' }))
         .catch((err) => res.status(500).json(err));
     }
